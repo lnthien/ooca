4fddb18c3a30d58ef24f2c82af948a770d8bc5ef
hendrick <hien.le@websparks.sg>
2023-03-02 15:10:01 +0700

    OOCA-TASK#2: Update e2e/unit test

--- src/stress/stress.controller.spec.ts
+++ src/stress/stress.controller.spec.ts
@@ -1,18 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { StressController } from './stress.controller';
+import { StressModule } from './stress.module';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
 
 describe('StressController', () => {
-  let controller: StressController;
+  let app: INestApplication;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [StressController],
+      imports: [StressModule],
     }).compile();
 
-    controller = module.get<StressController>(StressController);
+    app = module.createNestApplication();
+    await app.init();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('Get all records successfully', async () => {
+    await request(app.getHttpServer()).get('/stress').expect(200);
+  });
+
+  it('Create new record successfully', async () => {
+    await request(app.getHttpServer())
+      .post('/stress')
+      .send({
+        stressLevel: 0,
+        associateImg: '',
+      })
+      .expect(201);
+    // .expect({ stressLevel: 0, associateImg: '', createdAt: isDate});
   });
 });
